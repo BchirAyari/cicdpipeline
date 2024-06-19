34afd2a2492f58d7d9ae31d91d2aaf096e141a80
Bechir <ayari.bechir.contact@gmail.com>
2024-06-19 11:04:47 +0200

    set parameters for karma config js

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,23 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
 
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.spec.ts'
+      { pattern: 'src/**/*.spec.ts', watched: false, type: 'module' } // Ajoutez cette ligne
     ],
 
 
@@ -27,13 +38,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
     preprocessors: {
+      'src/**/*.spec.ts': ['webpack'] // Ajoutez cette ligne
     },
-
-
+    mime: {
+      'text/x-typescript': ['ts', 'tsx'] // Ajoutez cette ligne
+    },
+    
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/pfe'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'kjhtml'],
 
 
     // web server port
@@ -61,7 +80,8 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-
+    
+    restartOnFileChange: true,
     // Concurrency level
     // how many browser instances should be started simultaneously
     concurrency: Infinity
